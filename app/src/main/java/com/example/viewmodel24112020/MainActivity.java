7df056dae6ddadb841211ca0553c30bb31b64e12
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-03-09 18:52:34 +0700

    get location

--- app/src/main/java/com/example/viewmodel24112020/MainActivity.java
+++ app/src/main/java/com/example/viewmodel24112020/MainActivity.java
@@ -1,22 +1,46 @@
 package com.example.viewmodel24112020;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.LifecycleOwner;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    MyLifeCycleActivity myLifeCycleActivity;
+
+    int REQUEST_CODE_LOCATION = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Lifecycle aware component
 
-        myLifeCycleActivity = new MyLifeCycleActivity();
-        getLifecycle().addObserver(myLifeCycleActivity);
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(
+                    this ,
+                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE_LOCATION
+                    );
+        }else{
 
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_LOCATION){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            }else{
+                Toast.makeText(this, "Bạn chưa có quyền truy cập vào GPS của thiết bị", Toast.LENGTH_LONG).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
---
+++ app/src/main/java/com/example/viewmodel24112020/MyLocationManager.java
@@ -0,0 +1,7 @@
+package com.example.viewmodel24112020;
+
+import androidx.lifecycle.LifecycleObserver;
+
+public class MyLocationManager implements LifecycleObserver {
+
+}
