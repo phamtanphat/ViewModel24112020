a64ddd6b2be09e53f1ed16eff547ca74e7502837
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-03-11 18:35:06 +0700

    viewmodel

--- app/src/main/java/com/example/viewmodel24112020/MainActivity.java
+++ app/src/main/java/com/example/viewmodel24112020/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     TextView mTvResult;
     Button mBtnPlus;
     int mCount = 0;
+    MyViewModel myViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,11 +35,20 @@ public class MainActivity extends AppCompatActivity {
         mTvResult = findViewById(R.id.textViewResult);
         mBtnPlus = findViewById(R.id.buttonPlus);
 
+        myViewModel = new ViewModelProvider(this).get(MyViewModel.class);
+
+        myViewModel.getCount().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                mCount = integer;
+                mTvResult.setText("Count : " + integer);
+            }
+        });
 
         mBtnPlus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mTvResult.setText("Count : " + ++mCount);
+               myViewModel.setCount(++mCount);
             }
         });
 
--- app/src/main/java/com/example/viewmodel24112020/MyViewModel.java
+++ app/src/main/java/com/example/viewmodel24112020/MyViewModel.java
@@ -1,6 +1,21 @@
 package com.example.viewmodel24112020;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MyViewModel extends ViewModel {
+    MutableLiveData<Integer> mMutableInteger;
+
+    public MyViewModel() {
+        mMutableInteger = new MutableLiveData<>();
+    }
+
+    public void setCount(int count){
+        mMutableInteger.setValue(count);
+    }
+
+    public LiveData<Integer> getCount(){
+        return  mMutableInteger;
+    }
 }
